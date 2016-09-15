23fbb7858915c3f9db353c0e2c891e7d4f032a5d
Rodrigo M. Mariano <rodmariano13@gmail.com>
2016-09-14 21:27:17 -0300

    Gerando imagens exerc 1 e 2

--- exerc_02.py
+++ exerc_02.py
@@ -1,20 +1,23 @@
 
 # Exerc. 01) a)
 
+
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 SIZE_X = 1200
 SIZE_Y = 100
 
-#x = np.random.rand(SIZE_X) 
-#x = x - 0.5 
 
-x = np.arange(SIZE_X)
+x = np.random.rand(SIZE_X) 
+x = x - 0.5 
 
-y = []
+#x = np.arange(SIZE_X)
+y = np.array([])
 
 copy_x = list(x)
+
 
 while len(y) < SIZE_Y:
 
@@ -22,17 +25,17 @@ while len(y) < SIZE_Y:
 	sum_values = 0
 
 	while i < 12:
-		sum_values = sum_values + value copy_x.pop(0)
+		sum_values = sum_values + copy_x.pop(0)
 		i = i + 1
 
-	y.append(sum_values)
+	y = np.append(y, sum_values)
 
 
-print("\n\nx: ")
+print("\nvalues of x: ")
 print(x)
-print("\n\ny: ")
+print("\nvalues of y: ")
 print(y)
 
+plt.hist(y)
 
-#plt.hist(y)
-#plt.show()
+plt.show()
